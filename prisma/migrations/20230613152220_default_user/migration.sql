01ea3854deeffa75c71489c3ff3b063cce26fb8f
lenny.lin <lennylin98@gmail.com>
2023-06-29 00:04:06 +0800

    migrate database

--- prisma/migrations/20230613152220_default_user/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `role` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Enum(EnumId(0))` to `Enum(EnumId(0))`.
-
-*/
--- AlterTable
-ALTER TABLE `User` MODIFY `role` ENUM('ADMIN', 'USER') NOT NULL DEFAULT 'USER';

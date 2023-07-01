d8149f3e55b108a66e92ae16d7a860f64407a161
lenny.lin <lennylin98@gmail.com>
2023-07-01 18:22:15 +0800

    revert config settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,7 +37,16 @@ export default defineNuxtConfig({
     },
     nitro: {
         storage: {
-            data: { driver: 'vercelKV' }
+            'redis': {
+                driver: 'redis',
+                /* redis connector options */
+                port: 6379, // Redis port
+                host: "127.0.0.1", // Redis host
+                username: "", // needs Redis >= 6
+                password: "",
+                db: 0, // Defaults to 0
+                tls: {} // tls/ssl
+            }
         }
     },
     secret: "my-secret"
